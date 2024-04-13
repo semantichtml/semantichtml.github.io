b81c0fc63481074ff4f0069b778eb8571f0ab056
Gabriel Cozma <me@gabs.eu.org>
2024-04-13 13:07:36 +0300

    feat: Fix Theme (2)

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,8 @@
+import Layout from "aplos/no-blog/Layout.vue";
+import type { Theme } from "vitepress";
+import "aplos/no-blog";
+import "./custom.scss";
+
+export default {
+  Layout: Layout,
+} satisfies Theme;
